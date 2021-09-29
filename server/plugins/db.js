5c759679b1fd870194290a90173158c43d575189
codewxh <914067438@qq.com>
2021-09-29 23:00:14 +0800

    wxh

--- server/plugins/db.js
+++ server/plugins/db.js
@@ -1,7 +1,7 @@
 module.exports = app => {
 	const mongoose = require('mongoose')
 	// 本地Mongodb身份验证
-	mongoose.connect('mongodb://localhost:27017/node-vue-moba',{
+	mongoose.connect('mongodb://admin:123456@localhost:27017/node-vue-moba?authSource=admin',{
 		useNewUrlParser: true
 	})
 }

862d10614e52b53d5828d7679631d2c2fbb57f43
codewxh <914067438@qq.com>
2021-10-17 02:11:00 +0800

    '移動端初始化配置'

---
+++ mobile-h5/src/views/Home.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="home">
+    <p>1111111</p>
+    <ul>
+      <li>1</li>
+      <li>2</li>
+      <li>3</li>
+      <li>4</li>
+      <li>5</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  components: {
+  }
+}
+</script>
+<style scoped lang="scss">
+p{
+  color: red;
+  font-size: 16px;
+}
+</style>

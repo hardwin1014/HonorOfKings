21b91787338ebde6974acfead6f8a7964dd2aaad
codewxh <914067438@qq.com>
2021-11-04 20:32:07 +0800

    mobile complate

--- mobile-h5/src/router/index.js
+++ mobile-h5/src/router/index.js
@@ -6,13 +6,49 @@ Vue.use(VueRouter)
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: () => import('../views/Home.vue')
+    redirect: '/home',
+    component: () => import('@/views/Main/index'),
+    children: [
+      {
+        path: '/home',
+        name: 'home',
+        component: () => import('@/views/Main/Home/index')
+      },
+      // 文章详情页
+      {
+        path: '/article/:id',
+        name: 'articles',
+        component: () => import('@/views/Main/Article/index'),
+        props: true
+      },
+      // 全部英雄展示页面
+      {
+        path: '/all-hero',
+        name: 'allHeroes',
+        component: () => import('@/views/Main/Home/components/allHeroes'),
+      },
+      // 全部新闻展示页面
+      {
+        path: '/all-news',
+        name: 'allnews',
+        component: () => import('@/views/Main/Home/components/allNews'),
+      }
+    ]
+  },
+  // 英雄详情页
+  {
+    path: '/heroes/:id',
+    name: 'heroes',
+    component: () => import('@/views/Hero/index'),
+    props: true
   }
 ]
 
 const router = new VueRouter({
-  routes
+  routes,
+  scrollBehavior() {
+    return { x: 0, y: 0 }
+  }
 })
 
 export default router
